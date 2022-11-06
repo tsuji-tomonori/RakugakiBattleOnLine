8c7e0f33115b75e74dbbf57d12ac627d9934a1f9
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-11-06 15:45:48 +0900

    infoが取れないと総崩れしていたので全体的に見直し

--- src/dis_connect/lambda_function.py
+++ src/dis_connect/lambda_function.py
@@ -28,7 +28,7 @@ ep = EnvironParam.from_env()
 logger = logging.getLogger()
 logger.setLevel(ep.LOG_LEVEL)
 dynamodb = boto3.resource("dynamodb")
-apigw = boto3.client('apigatewaymanagementapi', endpoint_url=ep.ENDPOINT_URL)
+apigw = boto3.client("apigatewaymanagementapi", endpoint_url=ep.ENDPOINT_URL)
 user_table = dynamodb.Table(ep.USER_TABLE_NAME)
 room_table = dynamodb.Table(ep.ROOM_TABLE_NAME)
 
@@ -42,22 +42,27 @@ class UDbInfoSchema(NamedTuple):
         return UDbInfoSchema(**{k: response["Item"][k] for k in UDbInfoSchema._fields})
 
 
+class DoNotRetryException(Exception):
+    ...
+
+
 def all_delete_user_table(connection_id: str) -> None:
     res = user_table.query(KeyConditionExpression=Key(ep.USER_TABLE_PKEY).eq(connection_id))
     skeys = [item[ep.USER_TABLE_SKEY] for item in res["Items"]]
-    for skey in skeys:
-        user_table.delete_item(
-            Key={
-                ep.USER_TABLE_PKEY: connection_id,
-                ep.USER_TABLE_SKEY: skey,
-            }
-        )
+    try:
+        for skey in skeys:
+            user_table.delete_item(
+                Key={
+                    ep.USER_TABLE_PKEY: connection_id,
+                    ep.USER_TABLE_SKEY: skey,
+                }
+            )
+    except Exception as e:
+        logger.exception("delete_item_error")
+        raise DoNotRetryException from e
 
 
-def lambda_handler(event, context):
-    logger.info(json.dumps(event, indent=2))
-    connection_id = event["requestContext"]["connectionId"]
-    # 今後の処理のためinfoを取得しておく
+def get_info(connection_id: str) -> UDbInfoSchema | None:
     try:
         res_info = user_table.get_item(
             Key={
@@ -65,48 +70,69 @@ def lambda_handler(event, context):
                 ep.USER_TABLE_SKEY: "info",
             }
         )
-        info = UDbInfoSchema.from_db(res_info)
-    except:
-        logger.exception("user_table.get_item_error")
-        return {
-            "statusCode": 500,
-        }
-    # 自分の情報を削除する
+    except Exception as e:
+        logger.exception("get_item_error")
+        raise DoNotRetryException from e
     try:
-        all_delete_user_table(connection_id)
+        return UDbInfoSchema.from_db(res_info)
+    except:
+        return None
+
+
+def delete_room_table(room_id: str, connection_id: str) -> None:
+    try:
         room_table.delete_item(
             Key={
-                ep.ROOM_TABLE_PKEY: info.room_id,
+                ep.ROOM_TABLE_PKEY: room_id,
                 ep.ROOM_TABLE_SKEY: connection_id,
             }
         )
-    except:
+    except Exception as e:
         logger.exception("delete_item_error")
-        return {
-            "statusCode": 500,
-        }
-    # roomに入出したことを参加者に伝える
+        raise DoNotRetryException from e
+
+
+def post_room(info: UDbInfoSchema) -> None:
     try:
         items = room_table.query(
             KeyConditionExpression=Key(ep.ROOM_TABLE_PKEY).eq(info.room_id)
         )["Items"]
-        room_connection_ids = [item[ep.ROOM_TABLE_SKEY] for item in items]
-    except:
-        logger.exception("TableQueryError")
-        return {
-            "statusCode": 500,
-        }
-    try:
-        for room_connection_id in room_connection_ids:
+        connection_ids = [item[ep.ROOM_TABLE_SKEY] for item in items]
+    except Exception as e:
+        logger.exception("query")
+        raise DoNotRetryException from e
+    for connection_id in connection_ids:
+        try:
             apigw.post_to_connection(
                 Data=f"{info.user_name}さんが退出しました!".encode(),
-                ConnectionId=room_connection_id,
+                ConnectionId=connection_id,
             )
+        except boto3.client.exceptions.GoneException:
+            # 何らかの事情でDBに残っていても接続が切れている場合があるのでSkip
+            continue
+        except Exception as e:
+            logger.exception("delete_item_error")
+            raise DoNotRetryException from e
+
+
+def service(connection_id: str) -> None:
+    all_delete_user_table(connection_id)
+    info = get_info(connection_id)
+    if info is None:
+        return
+    delete_room_table(info.room_id, connection_id)
+    post_room(info)
+
+
+def lambda_handler(event, context):
+    logger.info(json.dumps(event, indent=2))
+    connection_id = event["requestContext"]["connectionId"]
+    try:
+        service(connection_id)
+        return {
+            "statusCode": 200,
+        }
     except:
-        logger.exception("post_to_connection_error")
         return {
             "statusCode": 500,
         }
-    return {
-        "statusCode": 200,
-    }
