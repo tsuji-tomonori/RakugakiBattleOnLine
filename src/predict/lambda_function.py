cb4b4de3342f62d8b0e0fc060253fe8a4daec5be
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-11-08 09:51:51 +0900

    コピペミス

--- src/predict/lambda_function.py
+++ src/predict/lambda_function.py
@@ -10,7 +10,6 @@ from typing import Any, NamedTuple
 
 import boto3
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
